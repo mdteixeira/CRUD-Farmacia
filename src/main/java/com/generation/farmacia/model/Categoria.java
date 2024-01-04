b1c20a26d793e57f69b79fc4124c5a9f906842f7
Matheus Teixeira <matheusdiegoteixeira@gmail.com>
2024-01-04 10:22:09 -0300

    CRUD categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id // define as primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // define o Auto_Increment
+	private Long id;
+
+	@Column(length = 500)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
